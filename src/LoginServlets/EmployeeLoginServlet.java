417051012aed9d1c1a8cd48a361909f84e6703c0
ghader-Salehi <amirghsl1380@gmail.com>
2020-07-01 00:56:00 -0700

    Design

--- src/LoginServlets/EmployeeLoginServlet.java
+++ src/LoginServlets/EmployeeLoginServlet.java
@@ -1,4 +1,15 @@
 package LoginServlets;
 
-public class EmployeeLoginServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class EmployeeLoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 }
