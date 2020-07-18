37c3eba1bfceb21d67ef91de1865a4e730d784b9
ali <a.forghani.y79@gmail.com>
2020-07-18 11:56:14 +0430

    trick 1.7 nullPoint EmployeeSalaryReceiptGetData.java

--- src/ServletsClasses/EmployeeSalaryReceiptGetData.java
+++ src/ServletsClasses/EmployeeSalaryReceiptGetData.java
@@ -6,25 +6,48 @@ import java.util.ArrayList;
 
 public class EmployeeSalaryReceiptGetData {
     public EmployeeSalaryReceiptGetData(String employeeNumber) {
-        this.employeeNumber = employeeNumber;
+        this.employeeCode = employeeNumber;
+        dbHelper = new DBHelper();
+        receipt = dbHelper.readReceipt(employeeNumber);
+        employee = dbHelper.readEmployee(employeeNumber);
+        jobInformation = employee.getJobInformation();
+        salaryInformation = employee.getSalaryInformation();
+        fullName = employee.getName() + employee.getLastName();
+        employeeCode = employeeNumber;
+        address = employee.getAddress();
+        baseSalary = salaryInformation.getBaseSalary();
+        finalSalary = receipt.get(receipt.size() - 1).getFinalSalary();
+        workTime = receipt.get(receipt.size() - 1).getWorkTime();
+        overTimeWork = receipt.get(receipt.size() - 1).getOverWorkTime();
+        addAmountForWorkSalary = String.valueOf(Integer.parseInt(receipt.get(receipt.size() - 1).getYearSalary()) - Integer.parseInt(salaryInformation.getBaseSalary()));
+        employeeType = jobInformation.getEmployeeType();
     }
 
-    String employeeNumber="1" ;
-    DBHelper dbHelper = new DBHelper();
-    ArrayList<Receipt> receipt = dbHelper.readReceipt(employeeNumber);
-    Employee employee = dbHelper.readEmployee(employeeNumber);
-    JobInformation jobInformation = employee.getJobInformation();
-    SalaryInformation salaryInformation = employee.getSalaryInformation();
+    DBHelper dbHelper;
+    ArrayList<Receipt> receipt;
+    Employee employee;
+    JobInformation jobInformation;
+    SalaryInformation salaryInformation;
+    String fullName;
+    String employeeCode;
+    String address;
+    String baseSalary;
+    String finalSalary;
+    String workTime;
+    String overTimeWork;
+    String addAmountForWorkSalary;
+    String employeeType;
 
-    String fullName = employee.getName()+employee.getLastName();
-    String employeeCode = employeeNumber;
-    String address = employee.getAddress();
-    String baseSalary= salaryInformation.getBaseSalary();
-    String finalSalary = receipt.get(receipt.size()-1).getFinalSalary();
-    String workTime = receipt.get(receipt.size()-1).getWorkTime();
-    String overTimeWork = receipt.get(receipt.size()-1).getOverWorkTime();
-    String addAmountForWorkSalary = String.valueOf(Integer.parseInt(receipt.get(receipt.size()-1).getYearSalary())-Integer.parseInt(salaryInformation.getBaseSalary()));
-    String employeeType = jobInformation.getEmployeeType();
+
+//    String fullName = employee.getName()+employee.getLastName();
+//    String employeeCode = employeeNumber;
+//    String address = employee.getAddress();
+//    String baseSalary= salaryInformation.getBaseSalary();
+//    String finalSalary = receipt.get(receipt.size()-1).getFinalSalary();
+//    String workTime = receipt.get(receipt.size()-1).getWorkTime();
+//    String overTimeWork = receipt.get(receipt.size()-1).getOverWorkTime();
+//    String addAmountForWorkSalary = String.valueOf(Integer.parseInt(receipt.get(receipt.size()-1).getYearSalary())-Integer.parseInt(salaryInformation.getBaseSalary()));
+//    String employeeType = jobInformation.getEmployeeType();
 
     public String getFullName() {
         return fullName;
